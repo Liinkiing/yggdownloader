8e5c2c63fd846479146a72f7bed80ea7b9fffb6b
Omar Jbara <omar.jbara2@gmail.com>
2023-01-21 02:17:57 +0100

    feat: allow download from torrent page

--- src/content.ts
+++ src/content.ts
@@ -1,40 +1,32 @@
-import { createFlash, createTorrentButton, findAllTorrentLinks } from '~/utils/dom'
-import { downloadTorrent } from '~/utils/ygg'
-import { AllDebrid, isValidFile } from '~/services/alldebrid'
+/* eslint-disable no-console */
+import { createTorrentButton, findAllTorrentLinks } from '~/utils/dom'
+import { downloadTorrentWithAllDebrid } from '~/utils/ygg'
 
 const isLoggedIn = document.querySelector('a#register') === null
+const downloadButtonTorrentPage = document.querySelector('a[href^="/engine/download_torrent"]')
+const isTorrentPage = downloadButtonTorrentPage !== null
 
 if (isLoggedIn) {
-  // eslint-disable-next-line
-  for (const link of findAllTorrentLinks()) {
+  if (isTorrentPage) {
     const button = createTorrentButton()
-    button.addEventListener('click', async () => {
-      const href = link.getAttribute('href') ?? ''
-      const hrefParts = href.split('/')
-      const id = (hrefParts[hrefParts.length - 1] ?? '').split('-')[0]
-      const resp = await downloadTorrent(id)
-      if (resp.ok) {
-        const torrent = await resp.blob()
-        const file = await AllDebrid.uploadTorrent(torrent)
-        if (isValidFile(file)) {
-          const flash = createFlash(`${file.name} added to AllDebrid.`)
-          if (file.ready) {
-            const status = await AllDebrid.getMagnetStatus(file.id)
-            if (status.links.length > 0) {
-              await navigator.clipboard
-                .writeText(status.links.map(l => l.link).join('\n'))
-                // eslint-disable-next-line no-alert
-                .catch(error => window.alert(error.message))
-              flash.textContent += ` ${status.links.length} links have been copied to your clipboard.`
-            }
-          }
-          document.body.append(flash)
-        }
-      } else {
-        const text = await resp.text()
-        throw new Error(text)
+    button.addEventListener('click', () => {
+      const id = downloadButtonTorrentPage?.getAttribute('href')?.split('=')[1]
+      if (id) {
+        downloadTorrentWithAllDebrid(id).catch(console.error)
       }
     })
-    link.parentNode?.append(button)
+    downloadButtonTorrentPage?.parentNode?.append(button)
+  } else {
+    // eslint-disable-next-line
+    for (const link of findAllTorrentLinks()) {
+      const button = createTorrentButton()
+      button.addEventListener('click', () => {
+        const href = link.getAttribute('href') ?? ''
+        const hrefParts = href.split('/')
+        const id = (hrefParts[hrefParts.length - 1] ?? '').split('-')[0]
+        downloadTorrentWithAllDebrid(id).catch(console.error)
+      })
+      link.parentNode?.append(button)
+    }
   }
 }
--- src/utils/ygg.ts
+++ src/utils/ygg.ts
@@ -1,4 +1,29 @@
+import { AllDebrid, isValidFile } from '~/services/alldebrid'
+import { createFlash } from '~/utils/dom'
+
 const BASE_URI = import.meta.env.VITE_YGG_BASE_URI
 
-export const downloadTorrent = (id: string) =>
-  fetch(`${BASE_URI}/engine/download_torrent?id=${id}`, { method: 'GET', body: null })
+export const downloadTorrentWithAllDebrid = async (id: string) => {
+  const resp = await fetch(`${BASE_URI}/engine/download_torrent?id=${id}`, { method: 'GET', body: null })
+  if (resp.ok) {
+    const torrent = await resp.blob()
+    const file = await AllDebrid.uploadTorrent(torrent)
+    if (isValidFile(file)) {
+      const flash = createFlash(`${file.name} added to AllDebrid.`)
+      if (file.ready) {
+        const status = await AllDebrid.getMagnetStatus(file.id)
+        if (status.links.length > 0) {
+          await navigator.clipboard
+            .writeText(status.links.map(l => l.link).join('\n'))
+            // eslint-disable-next-line no-alert
+            .catch(error => window.alert(error.message))
+          flash.textContent += ` ${status.links.length} links have been copied to your clipboard.`
+        }
+      }
+      document.body.append(flash)
+    }
+  } else {
+    const text = await resp.text()
+    throw new Error(text)
+  }
+}
