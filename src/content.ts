55a20bdfd457f84402850062ad0cc30790cdb28e
Omar Jbara <omar.jbara2@gmail.com>
2023-01-21 02:04:32 +0100

    feat: only display button if logged in

--- src/content.ts
+++ src/content.ts
@@ -2,35 +2,39 @@ import { createFlash, createTorrentButton, findAllTorrentLinks } from '~/utils/d
 import { downloadTorrent } from '~/utils/ygg'
 import { AllDebrid, isValidFile } from '~/services/alldebrid'
 
-// eslint-disable-next-line
-for (const link of findAllTorrentLinks()) {
-  const button = createTorrentButton()
-  button.addEventListener('click', async () => {
-    const href = link.getAttribute('href') ?? ''
-    const hrefParts = href.split('/')
-    const id = (hrefParts[hrefParts.length - 1] ?? '').split('-')[0]
-    const resp = await downloadTorrent(id)
-    if (resp.ok) {
-      const torrent = await resp.blob()
-      const file = await AllDebrid.uploadTorrent(torrent)
-      if (isValidFile(file)) {
-        const flash = createFlash(`${file.name} added to AllDebrid.`)
-        if (file.ready) {
-          const status = await AllDebrid.getMagnetStatus(file.id)
-          if (status.links.length > 0) {
-            await navigator.clipboard
-              .writeText(status.links.map(l => l.link).join('\n'))
-              // eslint-disable-next-line no-alert
-              .catch(error => window.alert(error.message))
-            flash.textContent += ` ${status.links.length} links have been copied to your clipboard.`
+const isLoggedIn = document.querySelector('a#register') === null
+
+if (isLoggedIn) {
+  // eslint-disable-next-line
+  for (const link of findAllTorrentLinks()) {
+    const button = createTorrentButton()
+    button.addEventListener('click', async () => {
+      const href = link.getAttribute('href') ?? ''
+      const hrefParts = href.split('/')
+      const id = (hrefParts[hrefParts.length - 1] ?? '').split('-')[0]
+      const resp = await downloadTorrent(id)
+      if (resp.ok) {
+        const torrent = await resp.blob()
+        const file = await AllDebrid.uploadTorrent(torrent)
+        if (isValidFile(file)) {
+          const flash = createFlash(`${file.name} added to AllDebrid.`)
+          if (file.ready) {
+            const status = await AllDebrid.getMagnetStatus(file.id)
+            if (status.links.length > 0) {
+              await navigator.clipboard
+                .writeText(status.links.map(l => l.link).join('\n'))
+                // eslint-disable-next-line no-alert
+                .catch(error => window.alert(error.message))
+              flash.textContent += ` ${status.links.length} links have been copied to your clipboard.`
+            }
           }
+          document.body.append(flash)
         }
-        document.body.append(flash)
+      } else {
+        const text = await resp.text()
+        throw new Error(text)
       }
-    } else {
-      const text = await resp.text()
-      throw new Error(text)
-    }
-  })
-  link.parentNode?.append(button)
+    })
+    link.parentNode?.append(button)
+  }
 }
